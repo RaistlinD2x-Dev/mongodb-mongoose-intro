7c66de712e2c3d651d6464a99545e4fc6ec2462b
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-07-03 18:37:51 -0500

    setup mongo connection in before hook

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.connection
-  .once('open', () => console.log('connection complete'))
-  .on('error', (error) => {
-    console.warn('Warning!', error);
+before(() => {
+  mongoose.connect('mongodb://localhost/users_test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
+  mongoose.connection
+    .once('open', () => {})
+    .on('error', (error) => {
+      console.warn('Warning!', error);
+    });
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
