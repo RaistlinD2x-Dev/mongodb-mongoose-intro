9440b3799d198d9e6986d29e670f185ab78bad7e
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-07-05 10:26:22 -0500

    used modifier for updating field without storing to application ram

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -40,5 +40,14 @@ describe('Updating records', () => {
 
   it('model class can find a record with id and update', (done) => {
     assertName(User.updateOne(joe._id, { name: 'Alex' }), done());
+  });
+
+  it('A user can have their postCount incremented by 1 using modifier', (done) => {
+    User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
